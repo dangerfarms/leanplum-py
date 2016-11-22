2021a5b0339c305cf459b6976cb926e5cb377d4f
Balint Vekerdy <balint@dangerfarms.com>
2016-11-22 16:13:26 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Danger Farms (Balint Vekerdy)',
     author_email='balint@dangerfarms.com',
     url='https://github.com/dangerfarms/leanplum-py',
-    download_url='https://github.com/dangerfarms/leanplum/tarball/0.1.0',
+    download_url='https://github.com/dangerfarms/leanplum-py/tarball/0.1.0',
     keywords=['leanplum', 'api', 'wrapper', 'marketing', 'push notification'],
     classifiers=[],
 )
